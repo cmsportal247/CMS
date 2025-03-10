b28aa4d8f141d9286c15263d56fa4ea35e00e504
cmsportal247 <portalcms22@gmail.com>
2025-03-10 16:15:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,8 @@ async function fetchCases() {
   const token = localStorage.getItem("token");
   try {
     // Fetch all cases (backend /cases endpoint does a scan)
-    const response = await fetch(`${apiBaseUrl}/CustomerCases`, {
+    const response = await fetch(`${apiBaseUrl}/cases
+    `, {
       headers: { "Authorization": `Bearer ${token}` }
     });
     if (response.ok) {
